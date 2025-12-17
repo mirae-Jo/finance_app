96369a9c1205aaf0d6d5c340f4b6d85b44e82d26
mirae <mirae9873@gmail.com>
2025-12-17 14:38:36 +0900

    fix: 주석 삭제, BookmarkButton 컴포넌트 다시 테일윈드 문법으로 cursor:pointer 옵션 추가

--- src/app/components/BookmarkButton.tsx
+++ src/app/components/BookmarkButton.tsx
@@ -2,7 +2,7 @@
 
 import { useStockStore } from "@/app/lib/store/useStockStore";
 import Image from "next/image";
-// 꽉 찬 별(북마크) 이미지도 필요합니다. 일단 있는 거랑, 가상의 이미지를 쓴다고 가정할게요.
+
 import unBookmarkIcon from "../../../public/bookmark_border_24dp_C5C016.svg";
 import BookmarkIcon from "../../../public/bookmark_24dp_EDE60C.svg";
 
@@ -11,20 +11,14 @@ interface Props {
 }
 
 export default function BookmarkButton({ symbol }: Props) {
-  // ✨ 주스탠드 훅은 여기서만 씁니다!
   const bookmarks = useStockStore((state) => state.bookmarks);
   const toggleBookmark = useStockStore((state) => state.toggleBookmark);
 
-  // 내 심볼이 즐겨찾기 목록에 있니?
   const isMark = bookmarks.includes(symbol);
 
   return (
-    <button
-      style={{ cursor: "pointer" }}
-      onClick={() => toggleBookmark(symbol)}>
+    <button className='cursor-pointer' onClick={() => toggleBookmark(symbol)}>
       <Image
-        // 즐겨찾기 상태에 따라 이미지를 바꾸거나 스타일을 줍니다.
-        // (지금은 이미지가 하나뿐이니, isFavorite일 때 css filter 등을 주거나 이미지를 교체해야 함)
         src={isMark ? BookmarkIcon : unBookmarkIcon}
         alt='bookmark toggle'
       />
