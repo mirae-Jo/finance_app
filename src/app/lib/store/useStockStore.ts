19cacabf37dc60212ce559ded3530d216cd11265
mirae <mirae9873@gmail.com>
2025-12-17 13:33:33 +0900

    feat: zustand 제공 미들웨어 persist 사용해보기

--- src/app/lib/store/useStockStore.ts
+++ src/app/lib/store/useStockStore.ts
@@ -1,7 +1,7 @@
+import { create } from "zustand";
 import { getStockData } from "@/app/action";
 import { Quote } from "@/app/type/types";
-
-import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface StockState {
   stocks: Quote[];
@@ -13,50 +13,58 @@ interface StockState {
   toggleBookmark: (symbol: string) => void;
 }
 
-export const useStockStore = create<StockState>((set, get) => ({
-  stocks: [],
-  updateTime: null,
+export const useStockStore = create<StockState>()(
+  persist(
+    (set, get) => ({
+      stocks: [],
+      updateTime: null,
 
-  fetchStocks: async () => {
-    const data = await getStockData([
-      "AAPL",
-      "TSLA",
-      "MSFT",
-      "AMZN",
-      "META",
-      "GOOGL",
-      "NVDA",
-      "NFLX",
-      "AVGO",
-      "AMD",
-      "PLTR",
-      "ORCL",
-    ]);
+      fetchStocks: async () => {
+        const data = await getStockData([
+          "AAPL",
+          "TSLA",
+          "MSFT",
+          "AMZN",
+          "META",
+          "GOOGL",
+          "NVDA",
+          "NFLX",
+          "AVGO",
+          "AMD",
+          "PLTR",
+          "ORCL",
+        ]);
 
-    if (!data) return;
+        if (!data) return;
 
-    set({
-      stocks: data.data,
-      updateTime: data.updateAt,
-    });
+        set({
+          stocks: data.data,
+          updateTime: data.updateAt,
+        });
 
-    const nextRefresh = 3600_000 - (Date.now() - data.updateAt);
+        const nextRefresh = 3600_000 - (Date.now() - data.updateAt);
 
-    setTimeout(() => {
-      get().fetchStocks();
-    }, nextRefresh);
-  },
+        setTimeout(() => {
+          get().fetchStocks();
+        }, nextRefresh);
+      },
 
-  bookmarks: [],
-  toggleBookmark: (symbol) =>
-    set((state) => {
-      // 이미 목록에 있는지 확인
-      const isExist = state.bookmarks.includes(symbol);
+      bookmarks: [],
+      toggleBookmark: (symbol) =>
+        set((state) => {
+          // 이미 목록에 있는지 확인
+          const isExist = state.bookmarks.includes(symbol);
 
-      return {
-        bookmarks: isExist
-          ? state.bookmarks.filter((item) => item !== symbol) // 있으면 -> 뺌 (삭제)
-          : [...state.bookmarks, symbol], // 없으면 -> 기존거 뒤에 붙임 (추가)
-      };
+          return {
+            bookmarks: isExist
+              ? state.bookmarks.filter((item) => item !== symbol) // 있으면 -> 뺌 (삭제)
+              : [...state.bookmarks, symbol], // 없으면 -> 기존거 뒤에 붙임 (추가)
+          };
+        }),
     }),
-}));
+    {
+      name: "stock-storage",
+      partialize: (state) => ({ bookmarks: state.bookmarks }),
+    }
+  )
+);
